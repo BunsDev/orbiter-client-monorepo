217981050281335ef56ede829cbb6dcd71048579
kakui <huanhui.boy@gmail.com>
2023-10-07 16:54:19 +0800

    change ecsFormat

--- libs/utils/src/lib/logger.ts
+++ libs/utils/src/lib/logger.ts
@@ -3,42 +3,40 @@ import DailyRotateFile from 'winston-daily-rotate-file';
 import ecsFormat from '@elastic/ecs-winston-format'
 export type OrbiterLogger = winston.Logger;
 export function loggerFormat() {
-  // return winston.format.combine(
-  //   winston.format.colorize(),
-  //   winston.format.timestamp(),
-  //   winston.format.json(),
-  //   // winston.format.printf(({ timestamp, level, message, stack, service }) => {
-  //   //   return `${timestamp} ${service || ""} [${level}]: ${message}\n${stack || ''}`;
-  //   // })
-  // );
-  // return winston.format.combine(
-  //   winston.format.timestamp(), // 
-  //   winston.format.json() // 
-  // )
-  return ecsFormat({
-    apmIntegration:true
-  });
+  return winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message, stack, service }) => {
+      return `${timestamp} ${service && service.name} [${level}]: ${message}\n${stack || ''}`;
+    })
+  );
 }
 
-export function createLoggerByName(context: string, meta: any = {}):OrbiterLogger {
+export function createLoggerByName(context: string, meta: any = {}): OrbiterLogger {
   const dirName = __dirname.substring(__dirname.lastIndexOf('/') + 1);
-  const transports = [new winston.transports.Console(), new DailyRotateFile({
-    filename: `logs/${context || "app"}/app-%DATE%.log`,
-    datePattern: 'YYYY-MM-DD', //
-    maxSize: '20m',
-    maxFiles: '7d',
-  }),];
-  
+  const transports = [
+    new winston.transports.Console({
+      format: loggerFormat()
+    }),
+    new DailyRotateFile({
+      filename: `logs/${context || "app"}/app-%DATE%.log`,
+      datePattern: 'YYYY-MM-DD', //
+      maxSize: '20m',
+      format: ecsFormat({
+        apmIntegration: true
+      }),
+      maxFiles: '7d',
+    }),];
+
   const logger = winston.createLogger({
     level: 'debug',
-    format: loggerFormat(),
     defaultMeta: {
       ...Object.assign(meta, {
         service: {
           name: dirName
         }
       }),
-      context:context,
+      context: context,
     },
     transports,
   });
