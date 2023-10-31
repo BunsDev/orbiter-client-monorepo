904e783edfb68d06e92f1747f7e47a18e4668bfb
kakui <huanhui.boy@gmail.com>
2023-10-31 11:25:54 +0800

    builder data

--- apps/explore-DataRefinery/src/app/transaction/bridgeTransaction.builder.ts
+++ apps/explore-DataRefinery/src/app/transaction/bridgeTransaction.builder.ts
@@ -3,34 +3,49 @@ import { BridgeTransaction, Transfers as TransfersModel } from '@orbiter-finance
 @Injectable()
 export default class BridgeTransactionBuilder {
     async build(transfer: TransfersModel): Promise<typeof BridgeTransaction> {
-        // if Standard  new BridgeTransactionStandardBuilder().build()
-        // if OBSource new BridgeTransactionOBSourceBuilder().build()
-        // if RouterV3 new BridgeTransactionRouterV3Builder().build()
-        // if RouterV1 new BridgeTransactionRouterV1Builder().build()
-        // build other commom
-        throw new Error('unrealized')
+        // build other common 
+        const baseData = new StandardBuilder().build(transfer);
+        let builderData;
+        // if ('source') {
+        //     builderData = new EVMOBSourceContractBuilder().build(transfer);
+        // } else if ('RouterV3') {
+        //     builderData = new EVMRouterV3ContractBuilder().build(transfer);
+        // }
+        throw Object.assign(baseData, builderData)
     }
 }
-export class BridgeTransactionStandardBuilder {
-    build(transfer: TransfersModel): Promise<typeof BridgeTransaction> {
-        throw new Error('unrealized')
-    }
-}
-
-export class BridgeTransactionOBSourceBuilder {
-    build(transfer: TransfersModel): Promise<typeof BridgeTransaction> {
-        throw new Error('unrealized')
-    }
-}
-
-export class BridgeTransactionRouterV3Builder {
-    build(transfer: TransfersModel): Promise<typeof BridgeTransaction> {
+export class StandardBuilder {
+    build(transfer: TransfersModel): Promise<Partial<typeof BridgeTransaction>> {
         throw new Error('unrealized')
     }
 }
 
-export class BridgeTransactionRouterV1Builder {
-    build(transfer: TransfersModel): Promise<typeof BridgeTransaction> {
+export class LoopringBuilder {
+    build(transfer: TransfersModel): Promise<Partial<typeof BridgeTransaction>> {
+        throw new Error('unrealized')
+    }
+}
+
+export class EVMOBSourceContractBuilder {
+    build(transfer: TransfersModel): Promise<Partial<typeof BridgeTransaction>> {
+        throw new Error('unrealized')
+    }
+}
+
+export class StarknetOBSourceContractBuilder {
+    build(transfer: TransfersModel): Promise<Partial<typeof BridgeTransaction>> {
+        throw new Error('unrealized')
+    }
+}
+
+export class EVMRouterV3ContractBuilder {
+    build(transfer: TransfersModel): Promise<Partial<typeof BridgeTransaction>> {
+        throw new Error('unrealized')
+    }
+}
+
+export class EVMRouterV1ContractBuilder {
+    build(transfer: TransfersModel): Promise<Partial<typeof BridgeTransaction>> {
         throw new Error('unrealized')
     }
 }
