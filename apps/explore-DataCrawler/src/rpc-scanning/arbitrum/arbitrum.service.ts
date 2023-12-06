fb9f307d3593c8c1c43ce757d5e7655aa1675d1a
kakui <huanhui.boy@gmail.com>
2023-12-06 18:04:18 +0800

    fix ar fee

--- apps/explore-DataCrawler/src/rpc-scanning/arbitrum/arbitrum.service.ts
+++ apps/explore-DataCrawler/src/rpc-scanning/arbitrum/arbitrum.service.ts
@@ -8,7 +8,7 @@ export class ArbitrumRpcScanningService extends EVMRpcScanningV6Service {
     receipt: ethers.TransactionReceipt,
   ): Promise<string> {
     const gasUsed = receipt.gasUsed.toString();
-    const gasPrice = transaction.gasPrice.toString();
+    const gasPrice = receipt.gasPrice.toString();
     // const gasUsedForL1 = receipt['extra']['gasUsedForL1'];
     const fee = new BigNumber(gasUsed).multipliedBy(gasPrice).toFixed(0);
     // const l1Fee = new BigNumber(gasUsed).multipliedBy(gasUsedForL1).toFixed(0);
