f4fce29d5d0a00723ad5b107540e40bf21f81544
kakui <huanhui.boy@gmail.com>
2023-12-10 07:30:47 +0800

    report

--- apps/openapi/src/dealer/report/report.controller.ts
+++ apps/openapi/src/dealer/report/report.controller.ts
@@ -1,4 +1,13 @@
 import { Controller } from '@nestjs/common';
-
-@Controller('report')
-export class ReportController {}
+import { Post, Body } from '@nestjs/common'
+@Controller('/dealer/report')
+export class ReportController {
+    @Post('tx')
+    index(@Body("hash") hash: string, @Body("channel") channel: string, @Body("desc") desc?: string) {
+        return {
+            hash,
+            channel,
+            desc
+        }
+    }
+}
