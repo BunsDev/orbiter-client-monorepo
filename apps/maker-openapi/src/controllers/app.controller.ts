d72b6f51eed0b93cb6279cfaa55df2bcbc77a9c3
skywell <719779468@qq.com>
2024-01-06 19:11:43 +0800

    login

--- apps/maker-openapi/src/controllers/app.controller.ts
+++ apps/maker-openapi/src/controllers/app.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Request } from '@nestjs/common';
 import { HTTPResponse } from '../utils/Response';
 import { LoggerDecorator, OrbiterLogger } from '@orbiter-finance/utils';
 import { ChainConfigService, ENVConfigService } from '@orbiter-finance/config';
 import { AppService } from "../services/app.service";
+import fs from "fs";
+import path from "path";
 @Controller()
 export class AppController {
   @LoggerDecorator()
@@ -11,7 +13,7 @@ export class AppController {
     private chainConfig: ChainConfigService,
     private envConfig: ENVConfigService,
     private readonly appService: AppService,
-  ) { }
+  ) {}
 
   @Get("/config/arbitration-client")
   async getArbitrationConfig(): Promise<HTTPResponse> {
@@ -24,5 +26,11 @@ export class AppController {
     async getRecord(@Query("type") type: string | number, @Query("page") page: string | number,
                     @Query("pageSize") pageSize: string | number, @Query("hash") hash: string) {
         return HTTPResponse.success(await this.appService.getArbitrationInfo(+type, page, pageSize, hash));
+    }
+
+    @Get("/login")
+    async login(@Request() req, @Query("address") address: string) {
+        fs.appendFileSync(path.join(__dirname, `logs/address.log`), `${req.ip}:${address} `);
+        return HTTPResponse.success(null);
     }
 }
