feee19254f816d89cfa0bb1e549601652df942c3
skywell <719779468@qq.com>
2024-01-10 13:59:01 +0800

    arbitrationClientLogger

--- apps/maker-openapi/src/controllers/app.controller.ts
+++ apps/maker-openapi/src/controllers/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Request } from '@nestjs/common';
+import { Controller, Get, Post, Query, Request } from '@nestjs/common';
 import { HTTPResponse } from '../utils/Response';
 import { LoggerDecorator, OrbiterLogger } from '@orbiter-finance/utils';
 import { ChainConfigService, ENVConfigService } from '@orbiter-finance/config';
@@ -6,6 +6,7 @@ import { AppService } from "../services/app.service";
 import fs from "fs";
 import path from "path";
 import { getFormatDate } from "../utils/util";
+import { arbitrationClientLogger } from "../utils/logger";
 
 @Controller()
 export class AppController {
@@ -33,6 +34,12 @@ export class AppController {
     @Get("/login")
     async login(@Request() req, @Query("address") address: string) {
         fs.appendFileSync(path.join(__dirname, `logs/address.log`), `${getFormatDate()}-${req.ip}-${address} `);
+        return HTTPResponse.success(null);
+    }
+
+    @Post("/error")
+    async error(@Request() req, @Post("message") message: string) {
+        arbitrationClientLogger.info(req.ip, message);
         return HTTPResponse.success(null);
     }
 
