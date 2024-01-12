b8bca9965bcb28f8d5f0d0cc7f989eef0e09372a
skywell <719779468@qq.com>
2024-01-12 12:01:54 +0800

    timezone

--- apps/maker-openapi/src/utils/util.ts
+++ apps/maker-openapi/src/utils/util.ts
@@ -2,7 +2,7 @@ import moment from "moment";
 
 export function getFormatDate(date?) {
     const timestamp = new Date(date || new Date().valueOf());
-    return moment(timestamp).utcOffset(getTimeZoneString(0)).format('YYYY-MM-DD HH:mm:ss');
+    return moment(timestamp).utcOffset(getTimeZoneString(8)).format('YYYY-MM-DD HH:mm:ss');
 }
 
 function getTimeZoneString(timeZone) {
