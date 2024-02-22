5c60e2ffde7758ac52b94c2a9966ba4feb87b71c
kakui <huanhui.boy@gmail.com>
2024-02-22 14:42:06 +0800

    change nonce

--- apps/maker-client/src/metric/metric.controller.ts
+++ apps/maker-client/src/metric/metric.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from "@nestjs/common";
+import { Controller, Get, Res,Query } from "@nestjs/common";
 import { PrometheusController } from "@willsoto/nestjs-prometheus";
 import { MetricService } from './metric.service';
 import { ChainConfigService, ENVConfigService } from "@orbiter-finance/config";
@@ -30,7 +30,6 @@ export class MetricController extends PrometheusController {
         }
         for (const chainId of chainIds) {
             for (const owner of owners) {
-
                 // read db history
                 const account = await this.accountFactoryService.createMakerAccount(
                     owner,
@@ -43,5 +42,45 @@ export class MetricController extends PrometheusController {
             }
         }
         return result;
+    }
+    @Get('/changeNonce')
+    async changeNonce(@Query() query:any) {
+        try {
+            const chainId = query['chainId'];
+            const value = +query['value'];
+            const owner = query['owner'];
+            if (!chainId) {
+                throw new Error('ChainId parameter missing')
+            }
+            if (!value) {
+                throw new Error('Value parameter missing')
+            }
+            if (!owner) {
+                throw new Error('Owner parameter missing')
+            }
+            const account = await this.accountFactoryService.createMakerAccount(
+                owner,
+                chainId
+            );
+            if (!account || !account.nonceManager) {
+                throw new Error('The initialized NonceManager instance was not obtained');
+            }
+            const current = await account.nonceManager.getLocalNonce();
+            const result = await account.nonceManager.setNonce(value);
+            return {
+                errno:0,
+                errmsg: 'success',
+                data: {
+                    current,
+                    last: value
+                }
+            }
+        }catch(error) {
+            return {
+                errno:1000,
+                errmsg: error.message,
+            }
+        }
+    
     }
 }
